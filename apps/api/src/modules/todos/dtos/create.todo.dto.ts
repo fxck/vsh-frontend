25209fa6386f8f7c75f8c37aea72bd97cfc8d69f
Ales Rechtorik <foxxcz@gmail.com>
2023-04-18 11:15:02 +0200

    swagger properties

--- apps/api/src/modules/todos/dtos/create.todo.dto.ts
+++ apps/api/src/modules/todos/dtos/create.todo.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsString } from 'class-validator';
 
 export class CreateTodoDto {
+  @ApiProperty({ description: 'The text of the todo item' })
   @IsString()
   readonly text: string;
 
+  @ApiProperty({ description: 'The completion status of the todo item', default: false })
   @IsBoolean()
   readonly completed: boolean;
 }
