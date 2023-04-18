420178ed6326ec2aba490a1bbc4aead28e0827fb
Ales Rechtorik <foxxcz@gmail.com>
2023-04-18 10:52:52 +0200

    add swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
@@ -10,12 +11,25 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
 
+    // Set up Swagger/OpenAPI
+  const config = new DocumentBuilder()
+    .setTitle('vsh frontend test api')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, document);
+
   const port = process.env.PORT || 3000;
 
   await app.listen(port);
 
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+
+  Logger.log(
+    `🚀 Swagger UI is running on: http://localhost:${port}/${globalPrefix}/docs`
   );
 }
 
