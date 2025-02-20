a11331fdb473e1774b4828914ee696e3a91340a2
Ales Rechtorik <foxxcz@gmail.com>
2025-02-20 15:09:58 +0100

    wip

--- apps/app/src/environments/environment.prod.ts
+++ apps/app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-e7f-3000.prg1.zerops.app/api',
+  apiUrl: '%%APP_URL%%',
   clientId: 'ar'
 };
--- apps/app/src/environments/environment.ts
+++ apps/app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: '%%APP_URL%%',
+  apiUrl: 'https://api-e7f-3000.prg1.zerops.app/api',
   clientId: 'ar'
 };
